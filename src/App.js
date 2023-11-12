acbba861a2b1606b2cb5d073e1184820886630c8
kmc6381 <127364818+kmc6381@users.noreply.github.com>
2023-11-12 15:48:59 +0900

    Header 구조 개선
    기존 Navbar에 있는 기능을 Header로 정리,배치를 하였음!

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-// import Layout from './layouts/Layout';
-// import Main from './pages/Main';
-// import MyPage from './pages/MyPage';
-// import Login from './pages/Login';
-// import TutorSearch from './pages/TutorSearch';
-// import TuteeSearch from './pages/TuteeSearch';
-// import FreeBoard from './pages/FreeBoard';
-// // import FreeBoard2 from './pages/FreeBoard2';
-// import SignUp from './pages/SignUp';
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Layout/>}>
-//           <Route inedx element ={<Main/>}/>
-//           <Route path="main" element ={<Main/>}/>
-//           <Route path="mypage" element ={<MyPage/>}/>
-//           <Route path="login" element ={<Login/>}/>
-//           <Route path="tutorsearch" element ={<TutorSearch/>}/>
-//        a   <Route path="tuteesearch" element ={<TuteeSearch/>}/>
-//           <Route path="freeboard" element ={<FreeBoard/>}/>
-//           {/* <Route path="freeboard2" element ={<FreeBoard2/>}/> */}
-//           <Route path="signup" element ={<SignUp/>}/>
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
@@ -40,7 +6,7 @@ import MyPage from './pages/MyPage';
 import Login from './pages/Login';
 import TutorSearch from './pages/TutorSearch';
 import TuteeSearch from './pages/TuteeSearch';
-import FreeBoard2 from './pages/FreeBoard2'; 
+import FreeBoard from './pages/FreeBoard'; 
 import SignUp from './pages/SignUp';
 
 
@@ -55,8 +21,7 @@ function App() {
                     <Route path="login" element={<Login />} />
                     <Route path="tutorsearch" element={<TutorSearch />} />
                     <Route path="tuteesearch" element={<TuteeSearch />} />
-                    
-                    <Route path="freeboard2" element={<FreeBoard2 />} />
+                    <Route path="freeboard" element={<FreeBoard />} />
                     <Route path="signup" element={<SignUp />} />
                 </Route>
             </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import './Header.css';
+import {NavLink} from 'react-router-dom';
 
 function Header() {
+    const activeStyle = {
+        backgroundColor: 'purple',
+        color: 'white'
+    }
+    
     return (
+        <div className="header">
+            <h1 style={{ color: '#9EE5E9' }}>TutorLink</h1>
+            <nav>
+                <ul>
+                    <li><NavLink to="/tutorsearch" style={({isActive}) => isActive? activeStyle: undefined}>튜터찾기</NavLink></li>
+                    <li><NavLink to="/tuteesearch" style={({isActive}) => isActive? activeStyle: undefined}>튜티찾기</NavLink></li>
+                    <li><NavLink to="/freeboard" style={({isActive}) => isActive? activeStyle: undefined}>자유게시판1</NavLink></li>
+                </ul>
+            </nav>
+
+            <div className="search-bar">
+                <input type="text" placeholder="검색" />
+               
+            </div>
+
+            {/* <div className="user-options">
+                <a href="#">로그인</a>
+                <a href="#">회원가입</a>
+            </div> */}
+
+               
+                <li><NavLink to="/login" style={({isActive}) => isActive? activeStyle: undefined}>로그인</NavLink></li>
+                <li><NavLink to="/signup" style={({isActive}) => isActive? activeStyle: undefined}>회원가입</NavLink></li>
+                <li><NavLink to="/freeboard2" style={({isActive}) => isActive? activeStyle: undefined}>자유게시판2</NavLink></li>
+                <li><NavLink to="/main" style={({isActive}) => isActive? activeStyle: undefined}>메인페이지</NavLink></li>
+                <li><NavLink to="/mypage" style={({isActive}) => isActive? activeStyle: undefined}>마이페이지</NavLink></li>
+                
+        </div>
+    );
+
+
         <div className="header">
             <h1 style={{ color: '#9EE5E9' }}>TutorLink</h1>
             <nav>
@@ -21,7 +58,29 @@ function Header() {
                 <a href="#">회원가입</a>
             </div>
         </div>
-    );
+    
+    
+
+    // return (
+    //     <div className="header">
+    //         <h1 style={{ color: '#9EE5E9' }}>TutorLink</h1>
+    //         <nav>
+    //             <ul>
+    //                 <li>튜터찾기</li>
+    //                 <li>튜티찾기</li>
+    //                 <li>자유게시판</li>
+    //             </ul>
+    //         </nav>
+    //         <div className="search-bar">
+    //             <input type="text" placeholder="검색" />
+               
+    //         </div>
+    //         <div className="user-options">
+    //             <a href="#">로그인</a>
+    //             <a href="#">회원가입</a>
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Header;
