b1d01c5e907d54a1e06af7ab5bfc54f94068be67
Kang-minchul <kmc6381@gmail.com>
2023-11-17 04:07:46 +0900

    Header 구조 개선 Layout에 Navbar 삭제 App.js 주석 삭제 및 FreeBoard2 삭제

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,14 +1,11 @@
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
     return(
         <>
             <Header/>
-            <Navbar/>
             <Outlet/>
-            {/* footer */}
         </>
     );
 }
