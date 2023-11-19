01fa6e0bc152b5e8df8e1c116c8f223d724aaad4
orihy97 <127364878+orihy97@users.noreply.github.com>
2023-11-19 15:38:20 +0900

    자유게시판 끝냈슴당

--- src/pages/FreeBoard.js
+++ src/pages/FreeBoard.js
@@ -1,30 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-function FreeBoard2() {
-    return (
-        <div>
-            <h1 style={{ color: '#9EE5E9' }}>자유게시판</h1>
-            <div className="search-bar">
-                <input type="text" placeholder="검색어를 입력해 보세요" />
-                <button style={{ backgroundColor: '#9EE5E9'}}>검색</button>
-            </div>
-            <button style={{ backgroundColor: '#9EE5E9'}}>글쓰기</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>번호</th>
-                        <th>제목</th>
-                        <th>작성자</th>
-                        <th>작성일자</th>
-                        <th>첨부파일</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* 테이블 데이터(동적)  */}
-                </tbody>
-            </table>
+const FreeBoard = () => {
+  const [articles, setArticles] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [showCreateForm, setShowCreateForm] = useState(false); // 새로운 글 작성 폼을 보여줄지 여부
+  const [newArticle, setNewArticle] = useState({
+    title: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    // 게시물을 가져옵니다.
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch("https://example.com/api/articles");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setArticles(data);
+      } catch (error) {
+        console.error("Error fetching articles:", error.message);
+      }
+    };
+    fetchArticles();
+  }, []);
+
+  const handleSearch = () => {
+    setArticles(articles.filter((article) => article.title.includes(searchKeyword)));
+  };
+
+  const handleCreate = () => {
+    // 여기서 새로운 글을 생성하는 API 호출 등의 로직을 추가할 수 있습니다.
+    // 예시로 로컬 상태에 새로운 글을 추가하고, 폼을 닫습니다.
+    setArticles([...articles, { id: Date.now(), title: newArticle.title, author: "CurrentUser", createdAt: new Date(), attachments: [] }]);
+    setShowCreateForm(false);
+    setNewArticle({
+      title: "",
+      content: "",
+    });
+  };
+
+  return (
+    <div>
+      <div style={{ backgroundColor: "white", border: "1px solid black" }}>
+        <h1>자유게시판</h1>
+        <div style={{ margin: 10 }}>
+          <input
+            type="text"
+            placeholder="검색어를 입력하세요"
+            value={searchKeyword}
+            onChange={(event) => setSearchKeyword(event.target.value)}
+          />
+          <button onClick={handleSearch}>검색</button>
+          <button onClick={() => setShowCreateForm(true)}>글쓰기</button>
         </div>
-    );
-}
+        <table>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일자</th>
+              <th>첨부파일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((article) => (
+              <tr key={article.id}>
+                <td>{article.id}</td>
+                <td>{article.title}</td>
+                <td>{article.author}</td>
+                <td>{article.createdAt}</td>
+                <td>{article.attachments.length > 0 ? "있음" : "없음"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* 글쓰기 폼 */}
+      {showCreateForm && (
+        <div style={{ marginTop: 20 }}>
+          <h2>새 글 작성</h2>
+          <label>
+            제목:
+            <input
+              type="text"
+              value={newArticle.title}
+              onChange={(e) => setNewArticle({ ...newArticle, title: e.target.value })}
+            />
+          </label>
+          <label>
+            내용:
+            <textarea
+              value={newArticle.content}
+              onChange={(e) => setNewArticle({ ...newArticle, content: e.target.value })}
+            />
+          </label>
+          <button onClick={handleCreate}>작성</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default FreeBoard2;
+export default FreeBoard;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-  id: yup.string().required().min(6).max(20),
-  password: yup.string().required().min(8).max(20).matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+|{}:"<>?,./;']+)/),
-  password2: yup.string().required().oneOf([yup.ref("password")]),
+  id: yup.string().required('아이디를 입력하세요').min(6, '아이디는 최소 6자 이상이어야 합니다').max(20),
+  password: yup.string().required().min(8).max(20).matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+|{}:"<>?,./;']+)/, "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다."),
+  password2: yup.string().required().oneOf([yup.ref("password"), null], '비밀번호가 일치하지 않습니다'),
   name: yup.string().required(),
   phone: yup.string().required().matches(/^01[0-9]{8}$/),
   email: yup.string().required().matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/),
@@ -13,7 +13,7 @@ const schema = yup.object().shape({
   gender: yup.string().required(),
 });
 
-const SignUpForm = () => {
+const SignUp = () => {
   const [values, setValues] = useState({
     id: "",
     password: "",
@@ -26,13 +26,17 @@ const SignUpForm = () => {
   });
 
   const onSubmit = (values, { setErrors }) => {
-    schema.validate(values)
+    schema.validate(values, { abortEarly: false })
       .then(() => {
         // 유효성 검사 통과 시 실행할 코드 작성
-        // ex: 회원가입 API 호출
+        // 회원가입 API 호출
       })
       .catch((err) => {
-        setErrors(err);
+        const errors = {};
+        err.inner.forEach(e => {
+          errors[e.path] = e.message;
+        });
+        setErrors(errors);
       });
   };
 
@@ -43,61 +47,94 @@ const SignUpForm = () => {
       onSubmit={onSubmit}
     >
       <Form>
-        <Field
-          name="id"
-          type="text"
-          placeholder="6~20자 영문, 숫자만 가능"
-          required
-        ></Field>
-        <Field
-          name="password"
-          type="password"
-          placeholder="8~20자 영문, 숫자, 특수문자 포함"
-          required
-        ></Field>
-        <Field
-          name="password2"
-          type="password"
-          placeholder="비밀번호를 다시 입력해주세요"
-          required
-        ></Field>
-        <Field
-          name="name"
-          type="text"
-          placeholder="이름을 입력해주세요"
-          required
-        ></Field>
-        <Field
-          name="phone"
-          type="text"
-          placeholder="휴대폰 번호를 입력해주세요"
-          required
-        ></Field>
-        <Field
-          name="email"
-          type="email"
-          placeholder="이메일 주소를 입력해주세요"
-          required
-        ></Field>
-         <Field
-          name="birth"
-          type="date"
-          placeholder="생년월일을 입력해주세요"
-          required
-        ></Field>
-        <Field
-          name="gender"
-          type="radio"
-          options={[
-            { value: "남자", label: "남자" },
-            { value: "여자", label: "여자" },
-          ]}
-          required
-        ></Field>
+        <div>
+          <label htmlFor="id">아이디</label>
+          <Field
+            name="id"
+            type="text"
+            placeholder="6~20자 영문, 숫자만 가능"
+            required
+          />
+          <ErrorMessage name="id" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <Field
+            name="password"
+            type="password"
+            placeholder="8~20자 영문, 숫자, 특수문자 포함"
+            required
+          />
+          <ErrorMessage name="password" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="password2">비밀번호 확인</label>
+          <Field
+            name="password2"
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요"
+            required
+          />
+          <ErrorMessage name="password2" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="name">이름</label>
+          <Field
+            name="name"
+            type="text"
+            placeholder="이름을 입력해주세요"
+            required
+          />
+          <ErrorMessage name="name" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="phone">전화번호</label>
+          <Field
+            name="phone"
+            type="text"
+            placeholder="휴대폰 번호를 입력해주세요"
+            required
+          />
+          <ErrorMessage name="phone" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <Field
+            name="email"
+            type="email"
+            placeholder="이메일 주소를 입력해주세요"
+            required
+          />
+          <ErrorMessage name="email" component="div" className="error-message" />
+        </div>
+        <div>
+          <label htmlFor="birth">생년월일</label>
+          <Field
+            name="birth"
+            type="date"
+            placeholder="생년월일을 입력해주세요"
+            required
+          />
+          <ErrorMessage name="birth" component="div" className="error-message" />
+        </div>
+        <div>
+          <label>성별</label>
+          <div role="group" aria-labelledby="gender-group">
+            <label>
+              <Field type="radio" name="gender" value="남자" />
+              남자
+            </label>
+            <label>
+              <Field type="radio" name="gender" value="여자" />
+              여자
+            </label>
+          </div>
+          <ErrorMessage name="gender" component="div" className="error-message" />
+        </div>
         <button type="submit">가입하기</button>
       </Form>
     </Formik>
   );
 };
 
-export default SignUpForm;
+export default SignUp;
