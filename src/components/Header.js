0ff89d04dcd3a24c0f6f9189ca5b0ca65914236f
Kang-minchul <kmc6381@gmail.com>
2023-11-19 15:25:40 +0900

    Header.js 주석 제거 및 코드 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,31 @@ import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 function Header() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loggedInLinks = [
+    <li key="logout"><NavLink to="/logout">로그아웃</NavLink></li>,
+    <li key="mypage"><NavLink to="/mypage">마이페이지</NavLink></li>
+  ];
 
   const loggedOutLinks = [
     <li key="login"><NavLink to="/login">로그인</NavLink></li>,
     <li key="signup"><NavLink to="/signup">회원가입</NavLink></li>
   ];
 
-  const loggedInLinks = [
-    <li key="logout"><NavLink to="/logout">로그아웃</NavLink></li>,
-    <li key="mypage"><NavLink to="/mypage">마이페이지</NavLink></li>
-  ];
-
   const currentLinks = isLoggedIn ? loggedInLinks : loggedOutLinks;
+
+  const toggleLogin = () => {
+  
+    setIsLoggedIn(prevIsLoggedIn => !prevIsLoggedIn);
+  };
 
   return (
     <div className="header">
       <h1>TutorLink</h1>
+      <button onClick={toggleLogin}>
+        {isLoggedIn ? '로그아웃' : '로그인'}
+      </button>
       <nav>
         <ul>
           {currentLinks}
