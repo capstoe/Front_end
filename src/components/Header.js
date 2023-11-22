b5d103f74e641b8b424145998c9c70d2fee906f5
orihy97 <127364878+orihy97@users.noreply.github.com>
2023-11-22 14:51:48 +0900

    글쓰기 누룰때 첨부파일 추가할수 있게 기능구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 function Header() {
-    const activeStyle = {
-        backgroundColor: 'purple',
-        color: 'white'
-    };
 
-    return (
-        <div className="header">
-            <div className="logo-and-nav">
-                <h1 style={{ color: '#9EE5E9' }}>TutorLink</h1>
-                <nav>
-                    <ul>
-                    <li><NavLink to="/main" style={({isActive}) => isActive? activeStyle: undefined}>메인페이지</NavLink></li>
-                    <li><NavLink to="/tutorsearch" style={({isActive}) => isActive? activeStyle: undefined}>튜터찾기</NavLink></li>
-                    <li><NavLink to="/tuteesearch" style={({isActive}) => isActive? activeStyle: undefined}>튜티찾기</NavLink></li>
-                    <li><NavLink to="/freeboard" style={({isActive}) => isActive? activeStyle: undefined}>자유게시판</NavLink></li>
-                    <li><NavLink to="/mypage" style={({isActive}) => isActive? activeStyle: undefined}>마이페이지</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className="search-bar">
-                <input type="text" placeholder="검색" />
-            </div>
-            <div className="user-options">
-                <NavLink to="/login" activeStyle={activeStyle}>로그인</NavLink> <br/>
-                <NavLink to="/signup" activeStyle={activeStyle}>회원가입</NavLink>
-            </div>
-        </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loggedInLinks = [
+    <li key="logout"><NavLink to="/logout">로그아웃</NavLink></li>,
+    <li key="mypage"><NavLink to="/mypage">마이페이지</NavLink></li>
+  ];
+
+  const loggedOutLinks = [
+    <li key="login"><NavLink to="/login">로그인</NavLink></li>,
+    <li key="signup"><NavLink to="/signup">회원가입</NavLink></li>
+  ];
+
+  const currentLinks = isLoggedIn ? loggedInLinks : loggedOutLinks;
+
+  const toggleLogin = () => {
+  
+    setIsLoggedIn(prevIsLoggedIn => !prevIsLoggedIn);
+  };
+
+  return (
+    <div className="header">
+      <h1>TutorLink</h1>
+      <button onClick={toggleLogin}>
+        {isLoggedIn ? '로그아웃' : '로그인'}
+      </button>
+      <nav>
+        <ul>
+          {currentLinks}
+          <li><NavLink to="/tutorsearch">튜터찾기</NavLink></li>
+          <li><NavLink to="/tuteesearch">튜티찾기</NavLink></li>
+          <li><NavLink to="/freeboard">자유게시판</NavLink></li>
+          <div>
+            <input type="text" placeholder="검색" />
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
