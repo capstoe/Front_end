4cb8b931eba74753cff38d1bde27b24d159d53cf
Kang-minchul <kmc6381@gmail.com>
2023-11-30 11:36:54 +0900

    form 파일 생성 Store 구현 redux thunk 구현 리듀서 구현

---
+++ src/apis/APICalls.js
@@ -0,0 +1,27 @@
+
+
+export function signUpUser(userData) {
+    const requestURL = 'http://localhost:8080'; // 여기에 회원가입 API 주소를 넣어주세요
+
+    return async function SignUp(_dispatch, _getState) {
+        try {
+            const response = await fetch(requestURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+
+            if (!response.ok) {
+                throw new Error('회원가입에 실패했습니다.');
+            }
+
+            const result = await response.json();
+            // 회원가입 성공 시 추가적으로 처리할 부분
+
+        } catch (error) {
+            console.error('회원가입 오류:', error.message);
+        }
+    };
+}
