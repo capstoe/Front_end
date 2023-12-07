19286087bca6b375ee56f94f25ee89d2a91ddc6b
kim <kimsquid110@naver.com>
2023-12-07 10:52:12 +0900

    ㅇ

--- src/pages/PostForm.js
+++ src/pages/PostForm.js
@@ -21,17 +21,15 @@ const PostForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        제목:
+    <form onSubmit={handleSubmit} className="post-form">
+      <div className="form-group">
+        <label htmlFor="title">제목:</label>
         <input type="text" name="title" value={formData.title} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        내용:
+      </div>
+      <div className="form-group">
+        <label htmlFor="content">내용:</label>
         <textarea name="content" value={formData.content} onChange={handleChange}></textarea>
-      </label>
-      <br />
+      </div>
       <button type="submit">제출</button>
     </form>
   );
